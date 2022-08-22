9d378d75dc960aad4349edb7369c1e0b9f09a0b9
Meraj Hossain <mdmaraj1500@gmail.com>
2022-08-22 17:27:15 +0600

    number error handler added

--- js/app.js
+++ js/app.js
@@ -1,44 +1,69 @@
+// Player-1 onClick.
+
 document
   .getElementById("player-one-select")
   .addEventListener("click", function () {
     selectPlayer("player-one-select", "player-one-name");
   });
+
+// Player-2 onClick.
+
 document
   .getElementById("player-two-select")
   .addEventListener("click", function () {
     selectPlayer("player-two-select", "player-two-name");
   });
+
+// Player-3 onClick.
+
 document
   .getElementById("player-three-select")
   .addEventListener("click", function () {
     selectPlayer("player-three-select", "player-three-name");
   });
+
+// Player-4 onClick.
+
 document
   .getElementById("player-four-select")
   .addEventListener("click", function () {
     selectPlayer("player-four-select", "player-four-name");
   });
+
+// Player-5 onClick.
+
 document
   .getElementById("player-five-select")
   .addEventListener("click", function () {
     selectPlayer("player-five-select", "player-five-name");
   });
+
+// Player-6 onClick.
+
 document
   .getElementById("player-six-select")
   .addEventListener("click", function () {
     selectPlayer("player-six-select", "player-six-name");
   });
 
+// calculate onClick.
+
 document.getElementById("calculate").addEventListener("click", function () {
   const playerCost = getInputValueById("player-cost-input");
+  if (isNaN(playerCost)) {
+    alert("Please Provide a valid number.");
+    return;
+  } else {
+    const liTag = document.getElementsByTagName("li").length;
 
-  const liTag = document.getElementsByTagName("li").length;
+    const perPlayerCost = liTag * playerCost;
 
-  const perPlayerCost = liTag * playerCost;
-
-  const playerExpenses = document.getElementById("expense-amount");
-  playerExpenses.innerText = perPlayerCost;
+    const playerExpenses = document.getElementById("expense-amount");
+    playerExpenses.innerText = perPlayerCost;
+  }
 });
+
+// calculate total onClick.
 
 document
   .getElementById("calculate-total")
@@ -47,8 +72,13 @@ document
     const managerCost = getInputValueById("manager");
     const coachCost = getInputValueById("coach");
 
-    const calculateTotal = playerExpenses + managerCost + coachCost;
+    if (isNaN(managerCost) || isNaN(coachCost)) {
+      alert("Please Provide a valid number.");
+      return;
+    } else {
+      const calculateTotal = playerExpenses + managerCost + coachCost;
 
-    const total = document.getElementById("total");
-    total.innerText = calculateTotal;
+      const total = document.getElementById("total");
+      total.innerText = calculateTotal;
+    }
   });
